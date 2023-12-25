ebddd40bd5c8516990d5b181b64aedf177585218
IgBovt <140014814+IgBovt@users.noreply.github.com>
2023-12-25 20:31:44 +0200

    update js
    task-2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,23 @@
-console.log('message');
+const formRef = document.querySelector('.form');
+const btnRef = document.querySelector('.js-btn');
+const dataObject = {
+  checkbox: '',
+  input: '',
+};
+
+formRef.addEventListener('input', getValue);
+btnRef.addEventListener('click', getResult);
+
+function getValue(e) {
+  return (
+    (dataObject.checkbox = e.currentTarget.elements.state.value),
+    (dataObject.input = e.currentTarget.elements.delay.value)
+  );
+}
+
+function getResult(e) {
+  e.preventDefault();
+  const promise = new Promise(() => {
+    setTimeout(() => {}, dataObject.input);
+  });
+}
